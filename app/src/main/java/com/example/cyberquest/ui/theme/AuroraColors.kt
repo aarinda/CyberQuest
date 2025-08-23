af7277fc590b81675ae8417a47833cbfc7ded35b
aarinda <arnold.arinda@yahoo.com>
2025-08-23 16:22:36 -0700

    Add AuroraColors.kt with color definitions for the theme

---
+++ app/src/main/java/com/example/cyberquest/ui/theme/AuroraColors.kt
@@ -0,0 +1,9 @@
+package com.example.cyberquest.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AuroraGreen = Color(0xFF00FFB4)
+val AuroraTeal = Color(0xFF00FFC6)
+val AuroraBlue = Color(0xFF00CFFF)
+val AuroraPurple = Color(0xFF7F5FFF)
+val AuroraNight = Color(0xFF0B1D26)
